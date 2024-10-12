bd55fd2923cff5df12a831345b08d82b8e33a43c
Kimmi Rindel <kimmirindel@Kimmis-MacBook-Pro.local>
2024-10-12 17:44:25 +1300

    Fix removed method

--- cloudinary/cloudinary.go
+++ cloudinary/cloudinary.go
@@ -4,9 +4,13 @@ import (
 	"log"
 	"os"
 
+	"context"
+
 	"github.com/joho/godotenv"
 
 	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api"
+	"github.com/cloudinary/cloudinary-go/v2/api/admin"
 )
 
 // Cloudinary instance to be used across the package
@@ -30,14 +34,36 @@ func Init() {
 		log.Println("Running on Fly.io, using environment variables...")
 	}
 
-	var err error
 	// Initialize Cloudinary
+	var err error
 	cld, err = cloudinary.NewFromURL(cloudinaryURL)
 	if err != nil {
 		log.Fatalf("Failed to initialize Cloudinary: %v", err)
 	}
 
 	log.Println("Cloudinary initialized")
+}
+
+func GetAllImagesInFolder(folderName string) ([]api.BriefAssetResult, error) {
+	log.Println("Getting images in folder", folderName)
+
+	cld := GetCloudinary()
+
+	// Create a context
+	ctx := context.Background()
+
+	// TODO get by folder name!
+	resources, err := cld.Admin.Assets(ctx, admin.AssetsParams{})
+
+	if len(resources.Assets) == 0 {
+		log.Println("No images found in the folder:", folderName)
+	}
+
+	if err != nil {
+		log.Fatalf("Failed to list resources: %v", err)
+	}
+
+	return resources.Assets, nil
 }
 
 // Returns the Cloudinary instance for use in other parts of the package.
