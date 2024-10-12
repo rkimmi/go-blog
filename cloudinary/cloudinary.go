042ab8b3e317e581771fffd07bc75f8d58fc00e3
Kimmi Rindel <kimmirindel@Kimmis-MacBook-Pro.local>
2024-10-12 17:34:06 +1300

    Try merge files

--- cloudinary/cloudinary.go
+++ cloudinary/cloudinary.go
@@ -4,8 +4,13 @@ import (
 	"log"
 	"os"
 
-	"github.com/cloudinary/cloudinary-go/v2"
+	"context"
+
 	"github.com/joho/godotenv"
+
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/cloudinary/cloudinary-go/v2/api"
+	"github.com/cloudinary/cloudinary-go/v2/api/admin"
 )
 
 // Cloudinary instance to be used across the package
@@ -36,6 +41,28 @@ func Init() {
 	}
 
 	log.Println("Cloudinary initialized")
+}
+
+func GetAllImagesInFolder(folderName string) ([]api.BriefAssetResult, error) {
+	log.Println("Getting images in folder", folderName)
+
+	cld := GetCloudinary()
+
+	// Create a context
+	ctx := context.Background()
+
+	// TODO get by folder name!
+	resources, err := cld.Admin.Assets(ctx, admin.AssetsParams{})
+
+	if len(resources.Assets) == 0 {
+		log.Println("No images found in the folder:", folderName)
+	}
+
+	if err != nil {
+		log.Fatalf("Failed to list resources: %v", err)
+	}
+
+	return resources.Assets, nil
 }
 
 // Returns the Cloudinary instance for use in other parts of the package.
