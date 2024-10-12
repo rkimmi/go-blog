890b7abe933b4c619a8995a700d106589f71f8f5
Kimmi Rindel <kimmirindel@Kimmis-MacBook-Pro.local>
2024-10-12 14:54:56 +1300

    Dont try load env on deployed envs

--- cloudinary/cloudinary.go
+++ cloudinary/cloudinary.go
@@ -1,41 +1,44 @@
 package cloudinary
 
 import (
-    "log"
-    "os"
+	"log"
+	"os"
 
-    "github.com/cloudinary/cloudinary-go/v2"
-    "github.com/joho/godotenv"
+	"github.com/cloudinary/cloudinary-go/v2"
+	"github.com/joho/godotenv"
 )
 
 // Cloudinary instance to be used across the package
 var cld *cloudinary.Cloudinary
 
 func Init() {
-    // Load environment variables from the .env file
-    err := godotenv.Load()
-    
-    if err != nil {
-        log.Fatalf("Error loading .env file")
-    }
+	// Load environment variables from the .env file
+	if os.Getenv("FLY_APP_NAME") == "" {
+		log.Println("Running locally, loading .env file...")
+		envErr := godotenv.Load()
+		if envErr != nil {
+			log.Fatal("Error loading .env file")
+		}
+	} else {
+		log.Println("Running on Fly.io, using environment variables...")
+	}
 
-    cloudinaryURL := os.Getenv("CLOUDINARY_URL")
-    if cloudinaryURL == "" {
-        log.Fatalf("CLOUDINARY_URL not set in environment")
-    }
+	cloudinaryURL := os.Getenv("CLOUDINARY_URL")
+	if cloudinaryURL == "" {
+		log.Fatalf("CLOUDINARY_URL not set in environment")
+	}
 
-    // Initialize Cloudinary
-    cld, err = cloudinary.NewFromURL(cloudinaryURL)
-    if err != nil {
-        log.Fatalf("Failed to initialize Cloudinary: %v", err)
-    }
+	// Initialize Cloudinary
+	_, err := cloudinary.NewFromURL(cloudinaryURL)
+	if err != nil {
+		log.Fatalf("Failed to initialize Cloudinary: %v", err)
+	}
 }
 
 // Returns the Cloudinary instance for use in other parts of the package.
 func GetCloudinary() *cloudinary.Cloudinary {
-    if cld == nil {
-        log.Fatalf("Cloudinary instance is not initialized. Call Init() first.")
-    }
-    return cld
+	if cld == nil {
+		log.Fatalf("Cloudinary instance is not initialized. Call Init() first.")
+	}
+	return cld
 }
-
